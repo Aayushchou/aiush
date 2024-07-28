d1af15402c058baae4a03e0086486c3b226e5d1e
aachou <aayushchou@gmail.com>
2024-07-29 00:09:38 +0100

    update train and run for training llama 3.1

--- src/aiush/run.py
+++ src/aiush/run.py
@@ -1,7 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from peft import PeftModel
 import torch
-
+from trl import setup_chat_format
 
 class ChatModel:
 
@@ -12,14 +12,13 @@ class ChatModel:
                                      torch_dtype=torch.float16)
 
         tokenizer = AutoTokenizer.from_pretrained(model_path)
-
         peft_model = PeftModel.from_pretrained(model, peft_path)
         peft_model = peft_model.merge_and_unload()
-
+        
         tokenizer.padding_side = "right"
         tokenizer.pad_token = tokenizer.eos_token
 
-        self.pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+        self.pipe = pipeline("text-generation", model=peft_model, tokenizer=tokenizer)
 
         self.messages = [
                 {
@@ -38,7 +37,7 @@ class ChatModel:
         outputs = self.pipe(self.messages,
                        max_new_tokens=256,
                        do_sample=True,
-                       temperature=0.7,
+                       temperature=0.1,
                        top_k=50,
                        eos_token_id=self.pipe.tokenizer.eos_token_id,
                        pad_token_id=self.pipe.tokenizer.pad_token_id
@@ -47,9 +46,9 @@ class ChatModel:
 
 if __name__ == "__main__":
     
-    model = ChatModel("facebook/opt-350m",
-                      "./models/opt350m-adapter",
-                      "you are a loving boyfriend whos romantic and talk in a sweet loving way")
+    model = ChatModel("meta-llama/Meta-Llama-3.1-8B-Instruct",
+                      "./models/meta-llama/Meta-Llama-3.1-8B-Instruct/",
+                      "You have high emotional intelligence, think step by step before responding. You are empathetic, loving and good at conversations. Don't finish your sentences with more than 2 emojis. Don't add extra long 'hahaha' at the end of your responses. Never repeat sentences or tokens")
     while True:
         msg = input("Me: ")
         if msg == "x":
--- src/aiush/train.py
+++ src/aiush/train.py
@@ -1,9 +1,10 @@
 from datasets import load_dataset
-from trl import SFTConfig, SFTTrainer
+from trl import SFTConfig, SFTTrainer, setup_chat_format
 from peft import LoraConfig
 from aiush.preprocess import ChatEntry
-from transformers import AutoModelForCausalLM, logging
+from transformers import AutoModelForCausalLM, AutoTokenizer, logging, BitsAndBytesConfig
 import json
+import torch
 import wandb
 import os
 from dotenv import load_dotenv
@@ -13,39 +14,69 @@ PWD = os.getcwd()
 
 def train(dataset_path: str, model_path: str, output_path: str = "models"):
     
-    
+    """set up output path"""    
     output_path = os.path.join(PWD, output_path, model_path)
     os.makedirs(output_path, exist_ok=True)
 
-    dataset = load_dataset("json", data_files=dataset_path, split="train") 
+    """load dataset"""
+    dataset = load_dataset("json", data_files=dataset_path, split='train')
+    
+    split_dataset = dataset.train_test_split(test_size=0.2)
+    train_dataset = split_dataset["train"]
+    test_dataset = split_dataset["test"]
+
+    """setup quantization config"""
+    bnb_config = BitsAndBytesConfig(
+                    load_in_4bit=True,
+                    bnb_4bit_use_double_quant=True,
+                    bnb_quant_type="nf4",
+                    bnb_4bit_compute_dtype=torch.bfloat16
+                )
+
+    """load model and tokenizer"""
+    model = AutoModelForCausalLM.from_pretrained(
+        model_path,
+        torch_dtype=torch.bfloat16,
+        quantization_config=bnb_config,
+        device_map="auto",
+        attn_implementation="flash_attention_2"
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    tokenizer.padding_side = "right"
+    tokenizer.pad_token = tokenizer.eos_token
+
+   
     peft_config = LoraConfig(
         r=16,
         lora_alpha=32,
         lora_dropout=0.05,
         bias="none",
+            target_modules="all-linear",
         task_type="CAUSAL_LM",
-    )
-
-    model = AutoModelForCausalLM.from_pretrained(
-        model_path,
-        load_in_4bit=True,
-        device_map="auto",
     )
 
     sft_config = SFTConfig(output_dir=os.path.join(PWD, output_path),
                            neftune_noise_alpha=5,
-                           num_train_epochs=1)
-
+                           num_train_epochs=1,
+                           logging_steps=100,
+                           bf16=True)
     trainer = SFTTrainer(
-        model,
+        model=model,
+        tokenizer=tokenizer,
         max_seq_length=256,
-        train_dataset=dataset,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
         args=sft_config,
-        peft_config=peft_config
+        peft_config=peft_config,
+        dataset_kwargs = {
+            "add_special_tokens": False,
+            "append_concat_token": False
+        }
     )
 
     print("training")
     trainer.train()
+
     trainer.model.save_pretrained(os.path.join(PWD, output_path))
 
 if __name__ == "__main__":
